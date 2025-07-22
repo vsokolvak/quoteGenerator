3f336778668a3017e347289960fb7c9085e28373
Vadim <vsokolvak2000@gmail.com>
2025-07-22 13:51:43 +0300

    add adaptive response style, button copy and toast message when copy quote

--- random-quotes/src/App.jsx
+++ random-quotes/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import QuoteBox from './components/QuoteBox'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
   return (
     <>
       <QuoteBox />
+      <ToastContainer position="top-center" autoClose={2000} />
     </>
   );
 }
--- random-quotes/src/components/QuoteBox.jsx
+++ random-quotes/src/components/QuoteBox.jsx
@@ -3,6 +3,7 @@ import quotes from "../data/quotes";
 import css from "./Style.module.css";
 import clsx from "clsx";
 import { getRandomColor } from "../utilits/getRandomColor";
+import { toast } from "react-toastify";
 
 const getRandomQuote = () => {
   const index = Math.floor(Math.random() * quotes.length);
@@ -24,15 +25,36 @@ const QuoteBox = () => {
     }, 200);
   };
 
+  const handleCopy = () => {
+    const fullText = `"${quote.text}" — ${quote.author}`;
+    navigator.clipboard
+      .writeText(fullText)
+      .then(() => {
+        toast.success(`Цитату ${fullText} скопійовано 📋`);
+      })
+      .catch(() => {
+        toast.error("Не вдалося скопіювати 😢");
+      });
+  };
+
   return (
     <div className={css.box} style={{ backgroundColor: bgColor }}>
-      <div className={css.quote + " " + clsx(isFading ? css.hideQuote : css.showQuote)}>
+      <div
+        className={
+          css.quote + " " + clsx(isFading ? css.hideQuote : css.showQuote)
+        }
+      >
         <p className={css.text}>'{quote.text}'</p>
         <p className={css.author}>— {quote.author}</p>
       </div>
       <button className={css.button} onClick={handleNewQuote}>
         Нова цитата
       </button>
+
+      <button className={css.copyButton} onClick={handleCopy}>
+        Скопіювати
+      </button>
+
     </div>
   );
 };
