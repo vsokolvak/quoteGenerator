bd2acbc73c5f18bb6b1dc13ce332c8741eb6ddf2
Vadim <vsokolvak2000@gmail.com>
2025-07-29 15:11:38 +0300

    add tooltip

--- random-quotes/src/components/QuoteBox/Quote/Quote.jsx
+++ random-quotes/src/components/QuoteBox/Quote/Quote.jsx
@@ -2,6 +2,8 @@ import { toast } from "react-toastify";
 import css from "./Style.module.css";
 import clsx from "clsx";
 import { useState } from "react";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
 
 const Quote = ({ text, author, isFading }) => {
 
@@ -30,10 +32,13 @@ const Quote = ({ text, author, isFading }) => {
       className={clsx(
         isFading ? css.hideQuote : css.showQuote,
         css.quote,
-        copied ? css.copied : ''
+        copied ? css.copied : ""
       )}
       onClick={handleCopy}
+      data-tooltip-id="my-tooltip"
+      data-tooltip-content="Click to copy!"
     >
+      <Tooltip id="my-tooltip" />
       <p className={css.text}>'{text}'</p>
       <p className={css.author}>— {author}</p>
     </div>
